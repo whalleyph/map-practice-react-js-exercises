b9d482ac606eb66bdcb527ae57a24279506fd52a
nbogie <neillbogie@googlemail.com>
2024-06-06 18:18:29 +0100

    tidy

--- src/MapPractice1.jsx
+++ src/MapPractice1.jsx
@@ -2,17 +2,14 @@ import { useEmojiGenerator } from "./useEmojiGenerator";
 
 export default function MapPractice1() {
     const emojisArray = useEmojiGenerator();
-
-    const divsArray = emojisArray.map((e) => (
-        <div key={e} className="emoji">
-            {e}
-        </div>
-    ));
+    console.log("emojisArray", emojisArray);
 
     return (
         <div className="mapPractice">
             <h2>Map Practice 1</h2>
-            <div className="emojiList">{divsArray}</div>
+            <div className="emojiList">
+                This is where your emojis need to go! (See expected1.html)
+            </div>
         </div>
     );
 }
--- src/MapPractice2.jsx
+++ src/MapPractice2.jsx
@@ -1,21 +1,15 @@
 import { useMovieGenerator } from "./useMovieGenerator";
 export default function MapPractice2() {
     const [movies, regenerate] = useMovieGenerator();
-
-    const divsArray = movies.map((movie) => (
-        <div key={movie.title} className="movie">
-            <h2>{movie.title}</h2>
-            <p>Star: {movie.star}</p>
-            <p>Genre: {movie.genre}</p>
-            <blockquote>&quot;{movie.quote}&quot;</blockquote>
-        </div>
-    ));
+    console.log("movies", movies);
 
     return (
         <div className="mapPractice">
             <h2>Map Practice 2</h2>
             <button onClick={regenerate}>Change Movies</button>
-            <div className="movieList">{divsArray}</div>
+            <div className="movieList">
+                This is where your movies need to go! (See expected2.html)
+            </div>
         </div>
     );
 }
